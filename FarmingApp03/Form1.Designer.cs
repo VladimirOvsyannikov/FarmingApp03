8794bd1ad892f7f02684935d096e36c362f040c3
VladimirOvsyannikov <Vovsyannikov4028@eagle.fgcu.edu>
2023-11-19 07:11:08 -0500

    Add project files.

---
+++ FarmingApp03/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace FarmingApp03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Cow");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Livestock-Area", new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Milk Storage");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Barn", new System.Windows.Forms.TreeNode[] {
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Root", new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Tractor");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Tiller");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Storage Building", new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode7});
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Drone");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Command Center", new System.Windows.Forms.TreeNode[] {
+            treeNode9});
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Soy Crop");
+            this.visualizationPanel = new System.Windows.Forms.Panel();
+            this.farmTreeView = new System.Windows.Forms.TreeView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.PopulateFarmBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // visualizationPanel
+            // 
+            this.visualizationPanel.AllowDrop = true;
+            this.visualizationPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.visualizationPanel.Location = new System.Drawing.Point(409, 48);
+            this.visualizationPanel.Name = "visualizationPanel";
+            this.visualizationPanel.Size = new System.Drawing.Size(650, 525);
+            this.visualizationPanel.TabIndex = 0;
+            this.visualizationPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.visualizationPanel_Paint);
+            // 
+            // farmTreeView
+            // 
+            this.farmTreeView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.farmTreeView.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.farmTreeView.HotTracking = true;
+            this.farmTreeView.Location = new System.Drawing.Point(39, 48);
+            this.farmTreeView.Name = "farmTreeView";
+            treeNode1.Name = "Node4";
+            treeNode1.Text = "Cow";
+            treeNode2.Name = "Node3";
+            treeNode2.Text = "Livestock-Area";
+            treeNode3.Name = "Node5";
+            treeNode3.Text = "Milk Storage";
+            treeNode4.Name = "Node1";
+            treeNode4.Text = "Barn";
+            treeNode5.Name = "Node0";
+            treeNode5.Text = "Root";
+            treeNode6.Name = "Node7";
+            treeNode6.Text = "Tractor";
+            treeNode7.Name = "Node8";
+            treeNode7.Text = "Tiller";
+            treeNode8.Name = "Node6";
+            treeNode8.Text = "Storage Building";
+            treeNode9.Name = "Node10";
+            treeNode9.Text = "Drone";
+            treeNode10.Name = "Node9";
+            treeNode10.Text = "Command Center";
+            treeNode11.Name = "Node11";
+            treeNode11.Text = "Soy Crop";
+            this.farmTreeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode8,
+            treeNode10,
+            treeNode11});
+            this.farmTreeView.Size = new System.Drawing.Size(331, 230);
+            this.farmTreeView.TabIndex = 1;
+            this.farmTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.farmTreeView_AfterSelect);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(39, 284);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Expand Tree\r\n";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // PopulateFarmBtn
+            // 
+            this.PopulateFarmBtn.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PopulateFarmBtn.Location = new System.Drawing.Point(237, 284);
+            this.PopulateFarmBtn.Name = "PopulateFarmBtn";
+            this.PopulateFarmBtn.Size = new System.Drawing.Size(133, 23);
+            this.PopulateFarmBtn.TabIndex = 3;
+            this.PopulateFarmBtn.Text = "PopulateFarm";
+            this.PopulateFarmBtn.UseVisualStyleBackColor = true;
+            this.PopulateFarmBtn.Click += new System.EventHandler(this.PopulateFarmBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Items / Item Containers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(404, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Visualization";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(20, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(194, 23);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Visit Item / Container";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(20, 140);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(230, 23);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Automatic Plant Watering";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(20, 178);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(297, 23);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Identify Unwanted Moving Objects";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(39, 352);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 221);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(312, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Arduino/Microbit Device Actions";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(20, 65);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(114, 23);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Scan Farm";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(20, 104);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(304, 23);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Check Soil Moisture / Crop Growth";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 585);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PopulateFarmBtn);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.farmTreeView);
+            this.Controls.Add(this.visualizationPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel visualizationPanel;
+        private System.Windows.Forms.TreeView farmTreeView;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button PopulateFarmBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ FarmingApp03/Form1.cs
@@ -0,0 +1,448 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FarmingApp03
+{
+    public partial class Form1 : Form
+    {
+        private VisualizationManager visualizationManager;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            visualizationManager = VisualizationManager.Instance;
+            visualizationManager.SetVisualizationPanel(visualizationPanel);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void visualizationPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // If the check box is checked, expand all the tree nodes.
+            if (checkBox1.Checked == true)
+            {
+                farmTreeView.ExpandAll();
+            }
+            else
+            {
+                // If the check box is not checked, collapse the first tree node.
+                farmTreeView.CollapseAll();
+            }
+        }
+
+        private void PopulateFarmBtn_Click(object sender, EventArgs e)
+        {
+            // Create a Crop container
+            ItemContainer crop = new ItemContainer("Crop", 3000.0, 290, 210, 150, 200, 20);
+            // Add the Crop container to the visualization manager
+            visualizationManager.AddContainer(crop);
+
+            // Create a Command Center container
+            ItemContainer commandCenter = new ItemContainer("Command Center", 7000.0, 25, 15, 250, 100, 60);
+            // Add the Command Center to the visualization manager
+            visualizationManager.AddContainer(commandCenter);
+
+            // Create a Drone item 
+            Item drone = new Item("Drone", 2150.0, 105, 35, 25, 25, 5);
+            // Add the Drone item to the Command Center container
+            commandCenter.AddItem(drone);
+
+            // Create a barn container
+            ItemContainer barnContainer = new ItemContainer("Barn", 10000.0, 25, 210, 200, 100, 80);
+            // Add the barn container to the visualization manager
+            visualizationManager.AddContainer(barnContainer);
+
+            // create a livestock container
+            ItemContainer livestockContainer = new ItemContainer("Livestock Area", 5000.0, 10, 1, 100, 50, 50);
+            // Add livestock container to the barn container
+            barnContainer.AddItem(livestockContainer);
+
+            // create a Milk Storage container
+            ItemContainer milkStorageContainer = new ItemContainer("Milk Storage", 1000.0, 90, 55, 40, 40, 50);
+            // Add Milk Storage container to the barn container
+            barnContainer.AddItem(milkStorageContainer);
+
+            // create a Cow item
+            Item cow = new Item("Cow", 500.0, 5, 5, 30, 20, 15);
+            // Add cow item to livestock container
+            livestockContainer.AddItem(cow);
+
+
+            // Display the updated structure
+            visualizationManager.DisplayStructure();
+        }
+
+        private void farmTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            /*
+            if (e.Node is IFormItem selectedFormItem)
+            {
+                // Enable UI elements for modifiction
+                enableModificaitonControls();
+
+                // Store the selected item or item container for later modification
+                selectedNode = e.Node.Tag as IFormItem;
+            }
+            else
+            {
+                // Disable UI elements for modification
+                disableModificationControls();
+
+                // Clear the selected item or item container
+                selectedNode = null;
+            }
+            */
+        }
+
+        private void enableModificationControls()
+        {
+            // Enable you UI elements for modification (eg. button)
+        }
+
+        private void disableModificationControls()
+        {
+            // Disablee your UI elements for modification (eg. button)
+        }
+    }
+}
+
+public interface IFormItem
+{
+    void AddItem(IFormItem item);
+    void RemoveItem(IFormItem item);
+    void Display(Control.ControlCollection controls);
+    void ChangeName(string newName);
+    void ChangePrice(double newPrice);
+    void ChangeLocation(int newX, int newY);
+    void ChangeSize(int newLength, int newWidth, int newHeight);
+    void DisplayStructure(TreeNodeCollection nodes);
+}
+
+
+public class ItemContainer : IFormItem
+{
+    private List<IFormItem> items;
+    private string name;
+    private double price;
+    private int locationX;
+    private int locationY;
+    private int length;
+    private int width;
+    private int height;
+
+    public ItemContainer(string name, double price, int locationX, int locationY, int length, int width, int height)
+    {
+        this.items = new List<IFormItem>();
+        this.name = name;
+        this.price = price;
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void AddItem(IFormItem item)
+    {
+        items.Add(item);
+    }
+
+    public void RemoveItem(IFormItem item)
+    {
+        items.Remove(item);
+    }
+
+    public void Display(Control.ControlCollection controls)
+    {
+        Panel containerPanel = new Panel
+        {
+            BorderStyle = BorderStyle.FixedSingle,
+            Width = length,
+            Height = width,
+            Left = locationX,
+            Top = locationY
+        };
+
+        Label nameLabel = new Label
+        {
+            Text = $"{name}",
+            Width = 100,
+            Height = 15,
+            Font = new Font("Arial", 8, FontStyle.Regular),
+            Left = locationX + length + 1,
+            Top = locationY + width/2 +1
+        };
+
+        // Additional details can be displayed here (e.g., location, size)
+        containerPanel.Controls.Add(nameLabel);
+
+        controls.Add(containerPanel);
+
+        containerPanel.Controls.Add(nameLabel);
+
+        controls.Add(nameLabel);
+        // Set the label to be visable
+        nameLabel.Visible = true;
+
+        foreach (var item in items)
+        {
+            item.Display(containerPanel.Controls);
+        }
+    }
+
+    public void ChangeName(string newName)
+    {
+        name = newName;
+    }
+
+    public void ChangePrice(double newPrice)
+    {
+        price = newPrice;
+    }
+
+    public void ChangeLocation(int newX, int newY)
+    {
+        locationX = newX;
+        locationY = newY;
+    }
+
+    public void ChangeSize(int newLength, int newWidth, int newHeight)
+    {
+        length = newLength;
+        width = newWidth;
+        height = newHeight;
+    }
+
+    public void DisplayStructure(TreeNodeCollection nodes)
+    {
+        TreeNode node = new TreeNode($"{name} (container)");
+        DisplayDetails(node.Nodes);
+
+        foreach (var item in items)
+        {
+            item.DisplayStructure(node.Nodes);
+        }
+
+        nodes.Add(node);
+    }
+
+    private void DisplayDetails(TreeNodeCollection nodes)
+    {
+        TreeNode node = new TreeNode($"Details: Location ({locationX}, {locationY}), Size {length}x{width}x{height}");
+        nodes.Add(node);
+    }
+}
+
+
+public class Item : IFormItem
+{
+    private string name;
+    private double price;
+    private int locationX;
+    private int locationY;
+    private int length;
+    private int width;
+    private int height;
+
+    public Item(string name, double price, int locationX, int locationY, int length, int width, int height)
+    {
+        this.name = name;
+        this.price = price;
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void AddItem(IFormItem item)
+    {
+        // Items cannot contain other items, so this method is not applicable for Item class
+        throw new InvalidOperationException("Items cannot contain other items.");
+    }
+
+    public void RemoveItem(IFormItem item)
+    {
+        // Items cannot contain other items, so this method is not applicable for Item class
+        throw new InvalidOperationException("Items cannot contain other items.");
+    }
+
+    public void Display(Control.ControlCollection controls)
+    {
+        Panel itemPanel = new Panel
+        {
+            BorderStyle = BorderStyle.FixedSingle,
+            Width = length,
+            Height = width,
+            Left = locationX,
+            Top = locationY
+        };
+
+        Label nameLabel = new Label
+        {
+            Text = $"{name}",
+            Width = 100,
+            Height = 15,
+            Font = new Font("Arial", 8, FontStyle.Regular),
+            Left = locationX + length + 1,
+            Top = locationY + width / 2 + 1
+        };
+
+        // Additional details can be displayed here (e.g., location, size)
+
+        itemPanel.Controls.Add(nameLabel);
+
+        controls.Add(itemPanel);
+
+        itemPanel.Controls.Add(nameLabel);
+
+        controls.Add(nameLabel);
+        // Set the label to be visable
+        nameLabel.Visible = true;
+    }
+
+    public void ChangeName(string newName)
+    {
+        name = newName;
+    }
+
+    public void ChangePrice(double newPrice)
+    {
+        price = newPrice;
+    }
+
+    public void ChangeLocation(int newX, int newY)
+    {
+        locationX = newX;
+        locationY = newY;
+    }
+
+    public void ChangeSize(int newLength, int newWidth, int newHeight)
+    {
+        length = newLength;
+        width = newWidth;
+        height = newHeight;
+    }
+
+    public void DisplayStructure(TreeNodeCollection nodes)
+    {
+        TreeNode node = new TreeNode($"{name} (Item)");
+        DisplayDetails(node.Nodes);
+        nodes.Add(node);
+    }
+
+    private void DisplayDetails(TreeNodeCollection nodes)
+    {
+        TreeNode node = new TreeNode($"Details: Location ({locationX}, {locationY}), Size {length}x{width}x{height}");
+        nodes.Add(node);
+    }
+}
+
+
+public class VisualizationManager
+{
+    private static VisualizationManager instance;
+    private Panel visualizationPanel;
+    private List<IFormItem> formItems;
+
+    private VisualizationManager()
+    {
+        formItems = new List<IFormItem>();
+    }
+
+    public static VisualizationManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new VisualizationManager();
+            }
+            return instance;
+        }
+    }
+
+    public void SetVisualizationPanel(Panel visualizationPanel_Paint)
+    {
+        visualizationPanel = visualizationPanel_Paint;
+    }
+
+    public void AddContainer(IFormItem formItem)
+    {
+        formItems.Add(formItem);
+    }
+
+    public void DeleteContainer(IFormItem formItem)
+    {
+        formItems.Remove(formItem);
+    }
+
+    public void DisplayStructure()
+    {
+        if (visualizationPanel != null)
+        {
+            visualizationPanel.Controls.Clear();
+
+            foreach (var formItem in formItems)
+            {
+                formItem.Display(visualizationPanel.Controls);
+            }
+        }
+        else
+        {
+            // Handle the case when visualizationPanel is not set
+            Console.WriteLine("Visualization panel is not set. Please set the visualization panel first.");
+        }
+    }
+
+    public void DisplayStructure(TreeView treeView)
+    {
+        if (treeView != null)
+        {
+            treeView.Nodes.Clear();
+
+            foreach (var formItem in formItems)
+            {
+                TreeNode parentNode = new TreeNode();
+                formItem.DisplayStructure(parentNode.Nodes);
+                treeView.Nodes.Add(parentNode);
+            }
+        }
+        else
+        {
+            Console.WriteLine("TreeView is not set. Please set the TreeView control first.");
+        }
+    }
+
+    public List<IFormItem> GetFromItems() 
+    { 
+        return formItems;
+    }
+
+    public void ModifyItemContainer(IFormItem itemContainer, Action<IFormItem> modification)
+    {
+        modification(itemContainer);
+    }
+
+    public void ModifyItem(IFormItem item, Action<IFormItem> modification)
+    {
+        modification(item);
+    }
+}
